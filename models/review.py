c981320adac40b05638b7c03adc98a3fea14e305
aasemgar7i <aasemgar7i@gamil.com>
2024-03-20 23:50:37 +0200

    recovery

--- models/review.py
+++ models/review.py
@@ -2,13 +2,17 @@
 """
 contains class Review
 """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
-
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """
     Review Class
     """
-    place_id = ''
-    user_id = ''
-    text = ''
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
