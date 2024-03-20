a21be08d7defe3f0a8eab014c9ab1b3413116a00
Elallagui99 <got.famme@gmail.com>
2024-03-20 21:09:51 +0000

    add review

--- models/review.py
+++ models/review.py
@@ -2,13 +2,17 @@
 """
 contains class Review
 """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlqlchemy imoprt Column, String, ForeignKey
 
 
 class Review(BaseModel):
     """
     Review Class
     """
-    place_id = ''
-    user_id = ''
-    text = ''
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
