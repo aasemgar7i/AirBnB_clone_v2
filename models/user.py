9ca45b67f759dd790522e41aa2ff7e7107f54a5a
aasemgar7i <aasemgar7i@gamil.com>
2024-03-20 17:12:57 +0200

    user file update

--- models/user.py
+++ models/user.py
@@ -3,13 +3,16 @@
 contains class User
 """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     User Class
     """
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
