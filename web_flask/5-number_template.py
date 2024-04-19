8823585968d6a2d829e9482a3a52309b9f8b6bb9
aasemgar7i <aasemgar7i@gamil.com>
2024-04-19 23:41:37 +0200

    update12

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,7 +10,8 @@ Routes:
     /number/<n>: Displays 'n is a number' only if <n> is an integer.
     /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
