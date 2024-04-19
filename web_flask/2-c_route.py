b2b065caa71b0e1c022be75cbd327b35bc2944b7
aasemgar7i <aasemgar7i@gamil.com>
2024-04-19 23:19:52 +0200

    update3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,9 +25,10 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c_text(text):
     """Display 'c +text'"""
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
