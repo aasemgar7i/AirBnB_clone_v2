a1209123389980287e579d1634dc6a35ac151ba8
aasemgar7i <aasemgar7i@gamil.com>
2024-04-19 23:30:07 +0200

    update7

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,7 +32,7 @@ def c_text(text):
     return 'C {}'.format(text)
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     """Display Python text"""
